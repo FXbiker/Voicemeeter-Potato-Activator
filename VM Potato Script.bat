000fcf6fb8d252a9af8c7a7f14fadb84a70116f5
FXbiker <87943932+FXbiker@users.noreply.github.com>
2023-04-17 13:28:55 +0100

    Python File

--- VM Potato Script.bat
+++
@@ -1,2 +0,0 @@
-reg add "HKEY_CURRENT_USER\VB-Audio\VoiceMeeter" /v code /t REG_DWORD /d 738879 /f
-start "" "C:\Program Files (x86)\VB\Voicemeeter\voicemeeter8x64.exe"
---
+++ VM Potato Script.py
@@ -0,0 +1,37 @@
+import datetime
+import os
+import winreg
+t = datetime.datetime.now().date()
+
+def regkey_value(path, name="", start_key=None):
+   if isinstance(path, str):
+      path = path.split("\\")
+   if start_key is None:
+      start_key = getattr(winreg, path[0])
+      return regkey_value(path[1:], name, start_key)
+   else:
+      subkey = path.pop(0)
+   with winreg.OpenKey(start_key, subkey) as handle:
+      assert handle
+      if path:
+         return regkey_value(path, name, handle)
+      else:
+         desc, i = None, 0
+         while not desc or desc[0] != name:
+            desc = winreg.EnumValue(handle, i)
+            i += 1
+         return desc[1]
+
+with open('data.txt', 'r', encoding='utf-8') as f:
+   f = f.read()
+   if f == "":
+      with open("data.txt", 'w', encoding='utf-8') as o:
+         o.write(str(t))
+   else:
+      nxt = datetime.datetime.strptime(f, str("%Y-%m-%d")).date()+ datetime.timedelta(days=32)
+      if nxt == t:
+         key = regkey_value(r"HKEY_CURRENT_USER\VB-Audio\VoiceMeeter", "code") + 32
+         print(key)
+         os.system(f'reg add "HKEY_CURRENT_USER\VB-Audio\VoiceMeeter" /v code /t REG_DWORD /d {key} /f')
+         with open("data.txt", 'w', encoding='utf-8') as o:
+            o.write(str(t))
